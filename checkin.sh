86d8aa9ee94a773404bca9f5c30e4d233a1159fe
xiezhengyun <zhengyun.xie@wowjoy.cn>
2023-04-17 16:56:35 +0800

    feat cookie2

--- checkin.sh
+++ checkin.sh
@@ -3,3 +3,10 @@ curl -H "cookie:${COOKIE}" -H 'content-type:application/json;charset=UTF-8' -d '
 echo '-----------------status-----------------'
 curl -H "cookie:${COOKIE}" -X GET 'https://glados.rocks/api/user/status' | grep -Eo '"leftDays":"[^"]*"'
 
+
+echo '------------------sign------------------'
+curl -H "cookie:${COOKIE2}" -H 'content-type:application/json;charset=UTF-8' -d '{"token": "glados.network"}' -X POST 'https://glados.rocks/api/user/checkin' | grep -Eo '"message":"[^"]*"'
+echo '-----------------status-----------------'
+curl -H "cookie:${COOKIE2}" -X GET 'https://glados.rocks/api/user/status' | grep -Eo '"leftDays":"[^"]*"'
+
+
